681c233fae4755f56f2be359969019ab762004bf
Kevin Kaspari <kevin.kaspari@gmail.com>
2020-10-15 19:10:04 -0400

    Add testcase for append. Add tests for prepend

--- linked-list/list_test.go
+++ linked-list/list_test.go
@@ -1,25 +1,47 @@
 package list
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
-
-var ll LinkedList
 
 func TestAppend(t *testing.T) {
 	tests := map[string]struct {
 		items []interface{}
 		want []interface{}
 	}{
-		"Nil list": {items: []interface{}{1,2,3}, want: []interface{}{1,2,3}},
+		"Single item": {items: []interface{}{1}, want: []interface{}{1}},
+		"Multiple items": {items: []interface{}{1,2,3}, want: []interface{}{1,2,3}},
 	}
 
 	for name, tc := range tests {
-		fmt.Println(tc)
+		var ll LinkedList
+
 		for _, item := range tc.items {
 			ll.Append(item)
+		}
+
+		got := ll.Contents()
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("%s: expected: %v, got: %v", name, tc.want, got)
+		}
+	}
+}
+
+func TestPrepend(t *testing.T) {
+	tests := map[string]struct {
+		items []interface{}
+		want []interface{}
+	}{
+		"One item": {items:[]interface{}{1}, want:[]interface{}{1}},
+		"Multiple items": {items: []interface{}{1,2,3}, want: []interface{}{3,2,1}},
+	}
+
+	for name, tc := range tests {
+		var ll LinkedList
+
+		for _, item := range tc.items {
+			ll.Prepend(item)
 		}
 
 		got := ll.Contents()
